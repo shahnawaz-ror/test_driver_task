2913b23bdddfc4019d7fec83c6df08a7ffa40c2d
shahanwaz <shahnawazkits@gmail.com>
2022-09-06 20:19:52 +0530

    Redirection added

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -9,7 +9,7 @@ Swagger::Docs::Config.register_apis({
                                       '1.0' => {
                                         controller_base_path: '',
                                         api_file_path: 'public/apidocs',
-                                        base_path: 'http://localhost:3000',
+                                        base_path: 'https://testdrivertask.herokuapp.com/',
                                         clean_directory: true
                                       }
                                     })
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  #resources :drivers
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
@@ -25,5 +25,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  #root "home#index"
+  root 'home#index'
 end
