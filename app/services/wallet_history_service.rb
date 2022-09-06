688055c37e1ae21325944cb69f3e70c6b316b474
shahanwaz <shahnawazkits@gmail.com>
2022-09-06 18:56:56 +0530

    first commit

---
+++ app/services/wallet_history_service.rb
@@ -0,0 +1,22 @@
+class WalletHistoryService
+  def initialize(driver_id)
+    @driver_id = driver_id
+  end
+
+  def add_wallet_history
+    driver = Driver.find(@driver_id[:driver_id])
+    card_detail = driver.card_details.default_true.first
+    transaction_histories = driver.transaction_histories.non_settled_transactions
+    if transaction_histories.present? && card_detail.present?
+      transaction_histories_ids = transaction_histories.ids.join(',')
+      total_amount = transaction_histories.map(&:amount).sum
+      wallet = Wallet.find_by_wallet_type(1)
+      wallet = Wallet.create(total_amount: total_amount, wallet_type: 1) if wallet.blank?
+      @wallet_history = driver.wallet_histories.where(driver_id: driver.id).update(total_amount: total_amount, transaction_histories_ids:transaction_histories_ids).first
+      @wallet_history = driver.wallet_histories.create(total_amount:total_amount, transaction_histories_ids: transaction_histories_ids, card_detail_id: card_detail.id, wallet_id: wallet.id) if @wallet_history.blank?
+      wallet = Wallet.find_by_wallet_type(1)
+      wallet_total_amount = wallet.wallet_histories.map(&:total_amount).sum
+      wallet.update(total_amount: wallet_total_amount)    
+    end
+  end
+end
